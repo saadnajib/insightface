3f7be186f030b69250a3ca8d0e81c67bb03e2086
saadnajib <saadnajib97@hotmail.com>
2021-08-24 22:02:10 +0500

    fixed boundingbox issue in the video inference file

--- inference_video.py
+++ inference_video.py
@@ -1,3 +1,4 @@
+from operator import truediv
 from torch.types import Number
 from backbones import get_model
 import torch.utils.data
@@ -27,6 +28,7 @@ from sklearn.preprocessing import Normalizer
 
 
 
+
 ##################################################################################################
 
 video_speedup = False
@@ -35,11 +37,90 @@ TOP_K = 1000
 NMS_THRESHOLD = 0.4
 VIEW_THRESHOLD = 0.9
 save_attendance = True
-save_attendance_path = "/home/saad/saad/arcface/insightface/recognition/arcface_torch/aligned_embeddings_omair_shared/saved_video/save_attendance"
+save_attendance_path = "/home/saad/saad/arcface/insightface/recognition/arcface_torch/aligned_embeddings_omair_shared/saved_video/save_attendance/new_saving_faces_cosface50_2video"
+
+file_name = "Recognitions_cosface100_saad"
+network='r50'
+# model = "ms1mv3_arcface_r50_fp16"
+# model = "ms1mv3_arcface_r18_fp16"
+# model = "ms1mv3_arcface_r100_fp16"
+# model = "glint360k_cosface_r50_fp16_0.1_new_trained"
+model = "glint360k_cosface_r50_fp16_0.1"
+# model = "glint360k_cosface_r34_fp16_0.1"
+# model = "glint360k_cosface_r18_fp16_0.1"
+# model = "glint360k_cosface_r100_fp16_0.1"
+generate_embedding = False
+# reg_embedding_dataset_path = '/home/saad/saad/arcface/insightface/recognition/arcface_torch/aligned_embeddings_omair_shared/gadoon_factory/folders_cosface100/'
+# reg_embedding_dataset_path = '/home/saad/saad/arcface/insightface/recognition/arcface_torch/aligned_embeddings_omair_shared/gadoon_factory/folders_cosface50_l2/'
+# reg_embedding_dataset_path = "/home/saad/saad/arcface/insightface/recognition/arcface_torch/aligned_embeddings_omair_shared/gadoon_factory/agligned_registration/aligned_faces_folders_cosface50_newtrainedmodel"
+reg_embedding_dataset_path = "/home/saad/saad/arcface/insightface/recognition/arcface_torch/aligned_embeddings_omair_shared/gadoon_factory/agligned_registration/aligned_faces_folders_cosface50"
+
+# input_video_path = "/home/saad/saad/arcface/insightface/recognition/arcface_torch/aligned_embeddings_omair_shared/gadoon_factory/videos/1.mp4"
+input_video_path = "/home/saad/saad/arcface/insightface/recognition/arcface_torch/aligned_embeddings_omair_shared/input_video/test3.mp4"
+save_video_flag = False
+save_video = "/home/saad/saad/arcface/insightface/recognition/arcface_torch/aligned_embeddings_omair_shared/saved_video/cosface50_1-th_trainedmodel.avi"
+landmark_flag = True
+bounding_box_flag = True
 
 ##################################################################################################
 
 # functions 
+
+def embeddings_gen(network,model,reg_embedding_dataset_path,resnet):
+    class CustomDataSet(Dataset):
+        def __init__(self, main_dir, transform):
+            self.main_dir = main_dir
+            self.transform = transform
+            all_folders = os.listdir(main_dir)
+            self.total_folders = all_folders
+
+        def __len__(self):
+            return len(self.total_folders)
+
+        def __getitem__(self, idx):
+            folder_loc = os.path.join(self.main_dir, self.total_folders[idx])
+            imgs = []
+            for item in os.listdir(folder_loc):
+                img_loc = os.path.join(folder_loc, item)    
+                if '.npy' in img_loc:
+                    continue
+                image = Image.open(img_loc).convert("RGB")
+                # tensor_image = self.transform(image)
+                imgs.append(image)
+            return imgs, self.total_folders[idx]
+
+    def collate_fn(x):
+        return [y for y in x]
+
+    data_path = reg_embedding_dataset_path
+    my_dataset = CustomDataSet(data_path, transform=torchvision.transforms.ToTensor())
+    train_loader = DataLoader(my_dataset , batch_size=8, shuffle=False, collate_fn=collate_fn,
+                                num_workers=1, drop_last=False)
+
+    print(device)
+
+    for idx, data in enumerate(train_loader):
+        for imgs,name in data:
+            print(len(imgs), name)
+            embeddings=[]
+            for img in imgs: #iterate over images of one folder
+                img = np.array(img) #RGB image
+                img = cv2.resize(img, (112,112))
+                # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                img = np.transpose(img, (2, 0, 1))
+                img = torch.from_numpy(img).unsqueeze(0).float()
+                img.div_(255).sub_(0.5).div_(0.5)
+                embedding = resnet(img.to(device))
+
+                in_encoder = Normalizer(norm='l2')
+                embedding = in_encoder.transform(embedding.detach().cpu().numpy()) 
+                
+                embeddings.append(embedding)
+            embeddings = np.vstack(embeddings)
+            path = os.path.join(data_path,name,network+"_embeddings.npy")
+            np.save(path, embeddings)
+            print(path)
+
 
 def convertTuple(tup):
     str =  '_'.join(tup)
@@ -123,30 +204,13 @@ def align_faces(dets, img):
     img = cv2.dnn.blobFromImage(img, scalefactor=1./255)
 
     aligned_faces = []  #np.array([], dtype=object)
+    bb = [] 
 
     for b in dets:
         if b[4] < VIEW_THRESHOLD:
             continue
-        # text = "{:.4f}".format(b[4])
         b = list(map(int, b))
-        # cv2.rectangle(img, (b[0], b[1]),
-        #                 (b[2], b[3]), (0, 0, 255), 2)
-
-        # cx = b[0]
-        # cy = b[1] + 12
-        # cv2.putText(img, text, (cx, cy),
-        #             cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
-
-        # landms
-        # cv2.circle(img, (b[5], b[6]), 1, (0, 0, 255), 4)
-        # cv2.circle(img, (b[7], b[8]), 1, (0, 255, 255), 4)
-        # cv2.circle(img, (b[9], b[10]), 1, (255, 0, 255), 4)
-        # cv2.circle(img, (b[11], b[12]), 1, (0, 255, 0), 4)
-        # cv2.circle(img, (b[13], b[14]), 1, (255, 0, 0), 4)
-
-
         roi = face_alignment.ROI(b)
-
         face = face_alignment.cut_roi(img, roi)
 
         lms = np.array([b[5:15]], dtype=np.float64).reshape(5,2)
@@ -159,20 +223,14 @@ def align_faces(dets, img):
         # cv2.imshow(" ",orig_face)
         # cv2.waitKey(1)
         
+        bb.append([roi.position.astype(int),(roi.position + roi.size).astype(int)])
         aligned_faces.append(orig_face)
 
-    return aligned_faces
+    return aligned_faces,bb
 
 ##################################################################################################
 
-file_name = "Recognitions_cosface100_saad"
-network='r100'
-# model = "ms1mv3_arcface_r50_fp16"
-# model = "ms1mv3_arcface_r18_fp16"
-# model = "ms1mv3_arcface_r100_fp16"
-# model = "glint360k_cosface_r50_fp16_0.1"
-# model = "glint360k_cosface_r18_fp16_0.1"
-model = "glint360k_cosface_r100_fp16_0.1"
+
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 weights='../weights/'+model+'/backbone.pth'
@@ -183,12 +241,19 @@ net = cv2.dnn.readNet("rfb.bin", "rfb.xml")
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_INFERENCE_ENGINE)
 print(device)
 
-f = open(file_name+".txt", "a")
-f.write("Model: "+model)
-f.close()
+# f = open(file_name+".txt", "a")
+# f.write("Model: "+model)
+# f.close()
 
 ##################################################################################################
 
+# Creating embedding and storing them in a file format in dataset directory
+
+if generate_embedding == True:
+    embeddings_gen(network,model,reg_embedding_dataset_path,resnet)
+
+
+##################################################################################################
 
 # Searching embeddings from the folders and storing it in a list / Loading Registration embeddings
 
@@ -197,8 +262,8 @@ print("Registered images")
 reg_embeddings = []
 names = [] 
 
-# directory = '/home/saad/saad/arcface/insightface/recognition/arcface_torch/aligned_embeddings_omair_shared/gadoon_factory/folders_cosface100/'
-directory = '/home/saad/saad/arcface/insightface/recognition/arcface_torch/aligned_embeddings_omair_shared/gadoon_factory/folders_cosface100_l2/'
+
+directory = reg_embedding_dataset_path
 for entry in os.scandir(directory):
     folder_flag = os.path.isdir(entry)
     if (folder_flag):
@@ -220,12 +285,9 @@ for entry in os.scandir(directory):
 
 ##################################################################################################
 
-
 # Loading Video
 
-video_path = "/home/saad/saad/arcface/insightface/recognition/arcface_torch/aligned_embeddings_omair_shared/input_video/test1.mp4"
-save_video = "/home/saad/saad/arcface/insightface/recognition/arcface_torch/aligned_embeddings_omair_shared/saved_video/cosface100_l2_1-throshold.avi"
-
+video_path = input_video_path
 
 # inital camera
 fourcc = cv2.VideoWriter_fourcc(*'MJPG')
@@ -235,7 +297,8 @@ cap.set(4,500)
 _, frame = cap.read()
 
 frame1 = resize_image(frame, 0.5)
-out = cv2.VideoWriter(save_video,cv2.VideoWriter_fourcc(*'MJPG'), 20.0, (frame1.shape[1], frame1.shape[0]), isColor=True)
+if save_video_flag == True:
+    out = cv2.VideoWriter(save_video,cv2.VideoWriter_fourcc(*'MJPG'), 20.0, (frame1.shape[1], frame1.shape[0]), isColor=True)
 total_frames_passed = 0
 
 
@@ -251,18 +314,19 @@ while cap.isOpened():
 
     if isSuccess:            
         try:
-            ######### TO write a frame #########
+            ######### TO write every frame #########
             # cv2.imwrite("/home/saad/saad/arcface/insightface/recognition/arcface_torch/aligned_embeddings_omair_shared/saved_video/saad.jpg",frame)
             
             # image = Image.fromarray(frame[...,::-1]) #bgr to rgb
             start_time = time.time()
             # frame = resize_image(frame, 0.5)
+            plain_frame = frame.copy()
 
             dets = get_detections(frame, net)
-            aligned_faces = align_faces(dets, frame)
+            aligned_faces,bb = align_faces(dets, frame)
 
             for i, face in enumerate(aligned_faces):
-                
+
                 ######### TO write cropped face image #########
                 # cv2.imwrite(save_path+""+name_list1[elem[0]]+"_"+str(elem[0])+"_"+str(i)+".jpg",255*face)
                 
@@ -288,12 +352,14 @@ while cap.isOpened():
                 for index2, e2 in enumerate(reg_embeddings):
                     for index3, e3 in enumerate(e2):
                         dists = np.linalg.norm(attendance_embedding - e3)
+                        # dists = np.dot(attendance_embedding,e3)/np.cross(np.linalg.norm(attendance_embedding), np.linalg.norm(e3))
                         distance.append(dists)
                 min_dist = min(distance)
                 index4 = distance.index(min_dist)
 
                 if min_dist <= 1.0:
                     Predicted_name = names[index4]
+                    check_name = Predicted_name
                     
                 else:
                     Predicted_name = "unknown"
@@ -303,28 +369,44 @@ while cap.isOpened():
                 print("score: ",min_dist)
                 print("\n")
 
-                #################################### printing bounding boxes and landmarks on frame #################################### 
-                for b in dets:
-                    if b[4] < VIEW_THRESHOLD:
-                        continue
-                    text = "{:.4f}".format(b[4])
-                    b = list(map(int, b))
-                    cv2.rectangle(frame, (b[0], b[1]),(b[2], b[3]), (0, 0, 255), 2)
-                    
-                    cx = b[0]
-                    cy = b[1] + 12
-                    cy1 = b[1] - 12
+                #################################### printing landmarks on frame #################################### 
+                if landmark_flag == True:
+                    for b in dets:
+                        if b[4] < VIEW_THRESHOLD:
+                            continue
+                        text = "{:.4f}".format(b[4])
+                        b = list(map(int, b))
 
-                    cv2.putText(frame, Predicted_name, (cx, cy),
-                                cv2.FONT_HERSHEY_DUPLEX, 0.7, (255, 255, 255),1)
+                        # landms
+                        cv2.circle(frame, (b[5], b[6]), 4, (0, 0, 255), -1)
+                        cv2.circle(frame, (b[7], b[8]), 4, (0, 255, 255), -1)
+                        cv2.circle(frame, (b[9], b[10]), 4, (255, 0, 255), -1)
+                        cv2.circle(frame, (b[11], b[12]), 4, (0, 255, 0), -1)
+                        cv2.circle(frame, (b[13], b[14]), 4, (255, 0, 0), -1)
 
-                    # landms
-                    cv2.circle(frame, (b[5], b[6]), 4, (0, 0, 255), -1)
-                    cv2.circle(frame, (b[7], b[8]), 4, (0, 255, 255), -1)
-                    cv2.circle(frame, (b[9], b[10]), 4, (255, 0, 255), -1)
-                    cv2.circle(frame, (b[11], b[12]), 4, (0, 255, 0), -1)
-                    cv2.circle(frame, (b[13], b[14]), 4, (255, 0, 0), -1)
+                #################################### printing bounding boxes  #################################### 
+                
+                if save_attendance == True:    
+                    frame_face_crop = plain_frame[bb[i][0][1]-70:bb[i][1][1]+70, bb[i][0][0]-70:bb[i][1][0]+70] 
 
+                if bounding_box_flag == True:
+                    cv2.rectangle(frame, bb[i][0],bb[i][1], (0, 0, 255), 2)
+
+                # cv2.imshow("",frame_face_crop)
+                
+                # bb[0][0][0] = b[0]
+                # bb[0][0][1] = b[1]
+                # bb[0][1][0] = b[2]
+                # bb[0][1][1] = b[3]
+
+                cx = bb[i][0][0]
+                cy = bb[i][0][1] + 12
+                cy1 = bb[i][0][1] - 12
+
+                cv2.putText(frame, Predicted_name, (cx, cy),cv2.FONT_HERSHEY_DUPLEX, 0.7, (255, 255, 255),1)
+
+                
+############################################
 
                 FPS = 1.0 / (time.time() - start_time)
 
@@ -337,20 +419,48 @@ while cap.isOpened():
                         cv2.LINE_AA)
 
                 if Predicted_name != "unknown" and save_attendance == True:
-                    cv2.imwrite(save_attendance_path+"/"+Predicted_name+"_score_"+str(min_dist)+".jpg",frame)
+                    
+                    name_list = []
+                    flag_solo = False
+
+                    # put any random image in solo folder to make this part of the code work
+
+                    photos = glob.glob(save_attendance_path+'/solo/*')
+                    #str3 = Ghulam_Mustafa_77796
+                    for p in photos:
+                        str1 = p.split('/')[-1].replace(' ', '').replace('.jpg', '')
+                        str2 = str1.split('_')[:3]
+                        str3 = convertTuple(str2)
+                        name_list.append(str3)
+                    
+                    for i in name_list:
+                        if check_name == i: 
+                            flag_solo = False
+                            break
+                        else:
+                            flag_solo = True
+    
+                    if flag_solo == True:
+                        cv2.imwrite(save_attendance_path+"/solo/"+Predicted_name+"_score_"+str(min_dist)+".jpg",frame_face_crop)
+                    cv2.imwrite(save_attendance_path+"/"+Predicted_name+"_score_"+str(min_dist)+".jpg",frame_face_crop)
          
         except:
             continue
         
-        frame1 = resize_image(frame, 0.5) 
-        out.write(frame1)
-        # frame1 = resize_image(frame, 0.75) 
-        cv2.imshow('video', frame1)
+        if save_video_flag == True:
+            frame1 = resize_image(frame, 0.5) 
+            out.write(frame1)
+        
+        frame2 = resize_image(frame, 0.5) 
+        cv2.imshow('video', frame2)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break  
 
 cap.release()
-out.release()
+
+if save_video_flag == True:
+    out.release()
+
 cv2.destroyAllWindows()
 
 
